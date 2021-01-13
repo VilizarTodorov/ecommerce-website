8c2459acc9e183d3e45fc00b8885e646c1768f67
VilizarTodorov <todorovvilizar@gmail.com>
2021-01-13 15:58:44 +0200

    added home component content and css

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,16 +1,29 @@
 import React from "react";
-import shopMens from '../../assets/shopMens.jpg'
-import shopWomen from '../../assets/shopWomens.jpg'
+import { Link } from "react-router-dom";
+import shopMens from "../../assets/shopMens.jpg";
+import shopWomen from "../../assets/shopWomens.jpg";
+import { MEN, WOMEN } from "../../constants/routes";
+import "./styles.scss";
 
 const Home = () => {
   return (
     <div className="home">
-      {/* <aside className='shop-men'> */}
-        {/* <img src={shopMens} alt="shop mens img"/> */}
-      {/* </aside> */}
-      {/* <aside className='shop-women'> */}
-        {/* <img src={shopWomen} alt="shop women img"/> */}
-      {/* </aside> */}
+      <aside style={{ backgroundImage: `url(${shopMens})` }} className="shop-main-category men">
+        <Link to={MEN}>
+          <div className="shop shop-men">
+            shop men
+            <i class="fas fa-arrow-right"></i>
+          </div>
+        </Link>
+      </aside>
+      <aside style={{ backgroundImage: `url(${shopWomen})` }} className="shop-main-category women">
+        <Link to={WOMEN}>
+          <div className="shop shop-women">
+            shop women
+            <i class="fas fa-arrow-right"></i>
+          </div>
+        </Link>
+      </aside>
     </div>
   );
 };
