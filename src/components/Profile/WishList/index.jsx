1958ee0585b92d08a9c8c7a6f4ee6298952e0a13
VilizarTodorov <todorovvilizar@gmail.com>
2021-02-01 23:04:04 +0200

    visualize wishlist products

--- src/components/Profile/WishList/index.jsx
+++ src/components/Profile/WishList/index.jsx
@@ -1,10 +1,29 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import Grid from "../../Grid";
+import ProductEntry from "../../ProductEntry";
 
-const WishList = (props) => {
+const wishlistSelector = (state) => state.user.wishList;
+
+const WishList = () => {
+  const wishlist = useSelector(wishlistSelector);
+
   return (
-    <div>
-      <h2>WishList</h2>
-    </div>
+    <Grid>
+      {wishlist.map((x) => (
+        <ProductEntry
+          key={x.id}
+          id={x.id}
+          mainImg={x.mainImg}
+          price={x.price}
+          name={x.name}
+          category={x.category}
+          sub={x.sub}
+          productType={x.productType}
+          otherColors={true}
+        ></ProductEntry>
+      ))}
+    </Grid>
   );
 };
 
